7f9120b8c430d6a35a788f25b946bd4263554d36
chait <chaitanya8178@gmail.com>
2025-01-05 14:51:14 -0500

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "C:/Users/chait/PycharmProjects/PythonProject/app1/todos.txt"
+
+
+def get_todos(filepath = FILEPATH):
+    """Read a text file and returns the list of to-do items"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """write the to-do items list to the todos.txt file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
